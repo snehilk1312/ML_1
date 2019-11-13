c02bdc167fd6549b7e935ba75a457e0321d4bfe0
snehil <44499005+snehilk1312@users.noreply.github.com>
2019-11-13 23:22:21 +0530

    Add files via upload

---
+++ Prehistoric Peublos/ex1.m
@@ -0,0 +1,83 @@
+fprintf('Plotting Data ...\n')
+data = load('ex1data1.txt');
+X = data(:, 1); y = data(:, 2);
+m = length(y); % number of training examples
+
+% Plot Data
+% Note: You have to complete the code in plotData.m
+plotData(X, y);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+
+J = computeCost(X, y, theta);
+fprintf('With theta = [0 ; 0]\nCost computed = %f\n', J);
+
+% further testing of the cost function
+J = computeCost(X, y, [-1 ; 2]);
+fprintf('\nWith theta = [-1 ; 2]\nCost computed = %f\n', J);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+fprintf('\ Running normalEqn ...\n')
+% run normalEqn
+theta = normalEqn(X, y, theta);
+
+% print theta to screen
+fprintf('Theta found by normalEqn:\n');
+fprintf('%f\n', theta);
+
+% Plot the linear fit
+hold on; % keep previous plot visible
+plot(X(:,2), X*theta, '-')
+legend('Training data', 'Linear regression')
+hold off % don't overlay any more plots on this figure
+
+% Predict values for population sizes of 35,000 and 70,000
+predict1 = [1, 2010] *theta;
+fprintf(' estimated year of final occupation 2010 %f\n',...
+    predict1*1);
+predict2 = [1, 1150] * theta;
+fprintf(' estimated year of final occupation %f\n',...
+    predict2*1);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+fprintf('Visualizing J(theta_0, theta_1) ...\n')
+
+% Grid over which we will calculate J
+theta0_vals = linspace(-10, 10, 100);
+theta1_vals = linspace(-1, 4, 100);
+
+% initialize J_vals to a matrix of 0's
+J_vals = zeros(length(theta0_vals), length(theta1_vals));
+
+% Fill out J_vals
+for i = 1:length(theta0_vals)
+    for j = 1:length(theta1_vals)
+	  t = [theta0_vals(i); theta1_vals(j)];
+	  J_vals(i,j) = computeCost(X, y, t);
+    end
+end
+
+
+% Because of the way meshgrids work in the surf command, we need to
+% transpose J_vals before calling surf, or else the axes will be flipped
+J_vals = J_vals';
+% Surface plot
+figure;
+surf(theta0_vals, theta1_vals, J_vals)
+xlabel('\theta_0'); ylabel('\theta_1');
+
+% Contour plot
+figure;
+% Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+xlabel('\theta_0'); ylabel('\theta_1');
+hold on;
+plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
