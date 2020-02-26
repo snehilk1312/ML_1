fdd59e5e8187ca2f61421559ea59fe7e2d53ca86
snehil <44499005+snehilk1312@users.noreply.github.com>
2020-02-26 21:53:11 +0530

    Add files via upload

---
+++ ML_AI_TechWithTim/knn/knn_working.py
@@ -0,0 +1,44 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv('car.data')
+print(data.head())
+
+
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data['buying']))
+maint = le.fit_transform(list(data['maint']))
+door = le.fit_transform(list(data['door']))
+persons = le.fit_transform(list(data['persons']))
+lug_boot = le.fit_transform(list(data['lug_boot']))
+safety = le.fit_transform(list(data['safety']))
+cls = le.fit_transform(list(data['class']))
+
+predict = "class"
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety))
+y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+model =KNeighborsClassifier(n_neighbors=11)
+
+model.fit(x_train, y_train)
+
+acc = model.score(x_test, y_test)
+
+print(acc)
+
+predicted = model.predict(x_test)
+
+names = ['unacc', 'acc', 'good', 'vgood']
+
+for i in range(len(x_test)):
+    print('Predicted :', names[predicted[i]], 'Data: ', x_test[i], 'Actual Value: ', names[y_test[i]])
+    n = model.kneighbors([x_test[i]], 5, True)
+    print("N: ", n)
+
